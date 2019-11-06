12ae77f541530417e802f63f21753e8663a7243b
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-11-06 03:35:13 -0600

    Updated trips.js such that a list of trips is dynamically rendered on the page when the user selects 'Specific Trip Stops' from the sidebar. User is then able to select the trip they'd like to view and be taken to that trip's stops#index page.

---
+++ app/assets/javascripts/stops.js
@@ -0,0 +1,11 @@
+$(function() {
+	listenForTripStopsClick();
+})
+
+function listenForTripStopsClick() {
+	$('a#user-trip-stops').on('click', function(event) {
+		event.preventDefault();
+		let userId = $('a#user-trip-stops')["0"]["name"];
+		getTripStops(userId)
+	})
+}
