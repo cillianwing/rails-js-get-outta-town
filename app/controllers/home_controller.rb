ea241942c66bf27756a2d20f3dc9746345dd8473
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-11-14 23:38:29 -0600

    Fixed HomeController#index so that it redirects a user to their index page if they attempt to go to the root path while already logged in.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
-  	@user = User.new
+  	if logged_in?
+  		redirect_to users_path
+  	else
+  		@user = User.new
+  		render :index
+  	end
   end
 end
