c879a32ec6287d7d8b04782b7b0396cf81475be3
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-11-26 18:24:17 -0600

    Created all required reviews view pages and necessary controller actions (no actual functioning code within these).

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,38 @@
 class ReviewsController < ApplicationController
+  before_action :set_user
+  before_action :require_login
+  skip_before_action :verify_authenticity_token
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comment, :rating, :user_id, :trip_id)
+  end
+
+  def set_user
+    @user = current_user
+  end
+
 end
