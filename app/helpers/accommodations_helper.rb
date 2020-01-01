65f5e62c793c09391ccac18095feca3a3d8e7add
Cillian Wing <cwing@zagmail.gonzaga.edu>
2020-01-01 13:17:15 -0600

    Created customized validation for Accommodation that confirms the checkin/out dates are within the dates of the current trip. Also, validations added for presence of key attributes.

--- app/helpers/accommodations_helper.rb
+++ app/helpers/accommodations_helper.rb
@@ -1,2 +1,34 @@
 module AccommodationsHelper
+  cattr_accessor :params
+
+  def accommodation_start_before_end
+    if self.check_in > self.check_out
+      errors.add(:check_in, "cannot be after check out.")
+    end
+  end
+
+  def accommodation_trip_date_confirm
+    @trip = Trip.find_by(id: params[:trip_id])
+    if self.check_out > @trip.end || self.check_out < @trip.start
+      errors.add(:new_accommodation, "cannot be outside the current trip's dates.")
+    elsif self.check_in < @trip.start || self.check_in > @trip.end 
+    	errors.add(:new_accommodation, "cannot be outside the current trip's dates.")
+    end
+  end 
+
+  def accommodations_date_confirm
+  	@trip = Trip.find_by(id: params[:trip_id])
+  	@accommodations = @trip.accommodations
+  	@accommodations.each do |accommodation|
+  		if accommodation.id != self.id 
+  			if self.check_in > accommodation.check_in && self.check_in < accommodation.check_out 
+  				errors.add(:new_accommodation, "cannot overlap with an existing accommodation.")
+  			elsif self.check_out > accommodation.check_in && self.check_out < accommodation.check_out 
+  				errors.add(:new_accommodation, "cannot overlap with an existing accommodation.")
+  			elsif self.check_in < accommodation.check_in && self.check_out > accommodation.check_out 
+  				errors.add(:new_accommodation, "cannot overlap with an existing accommodation.")
+  			end
+  		end
+    end
+  end		
 end
--- app/models/accommodation.rb
+++ app/models/accommodation.rb
@@ -1,4 +1,9 @@
 class Accommodation < ApplicationRecord
+	include AccommodationsHelper
+
   has_many :bookings
   has_many :trips, through: :bookings
+
+  validates :acc_type, :name, :address_1, :check_in, :check_out, :cost, :location, presence: true
+  validate :accommodation_start_before_end, :accommodation_trip_date_confirm, :accommodations_date_confirm
 end
